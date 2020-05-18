5cb27e76271ac8e78ed3bdd8097de25df66d536e
user1 <user1@users-MacBook.local>
2020-05-18 17:00:24 +0300

    hm8

---
+++ gallery.js
@@ -0,0 +1,47 @@
+import galleryItems from './gallery-items.js'
+
+const galleryList = document.querySelector(".js-gallery")
+const modal = document.querySelector(".js-lightbox")
+const modalImage = document.querySelector(".lightbox__image")
+const closeBtn = document.querySelector('button[data-action="close-lightbox"]')
+const closeClick = document.querySelector(".lightbox__content")
+
+
+galleryItems.forEach(imgObj => {
+  const listItem = `
+    <li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${imgObj.original}"
+    >
+      <img
+        class="gallery__image"
+        src="${imgObj.preview}"
+        data-source="${imgObj.original}"
+        alt="${imgObj.description}"/>
+    </a>
+  </li>
+  `
+  galleryList.insertAdjacentHTML("beforeend", listItem)
+})
+
+galleryList.addEventListener("click", openModal)
+
+function openModal(e) {
+  if (e.currentTarget === e.target) {
+    return;
+  }
+  event.preventDefault()
+    modal.classList.add("is-open")
+  let specialImage = e.target
+  const totalLink = specialImage.dataset.source;
+  modalImage.setAttribute("src", totalLink)
+}
+
+closeBtn.addEventListener("click", closeModal)
+closeClick.addEventListener("click", closeModal)
+
+function closeModal(e) {
+  modal.classList.remove("is-open")
+  modalImage.removeAttribute("src")
+}
